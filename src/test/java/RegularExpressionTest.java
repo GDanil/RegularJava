eace79a49217583065c5c10d0229f6d74e9be869
Danil <favurty@mail.ru>
2022-11-18 23:21:14 +0300

    Добавил первый тест "Завтрак в 09:00" к первому заданию

--- src/test/java/RegularExpressionTest.java
+++ src/test/java/RegularExpressionTest.java
@@ -3,11 +3,26 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class RegularExpressionTest {
+public class RegularExpressionTest
+{
 
     @Test
     public void TimeCheck_1correct()
     {
         Assert.assertTrue(RegularExpression.TimeCheck("Завртак в 09:00"));
+        Assert.assertTrue(RegularExpression.TimeCheck("Обед в 12:30."));
+        Assert.assertTrue(RegularExpression.TimeCheck("В субботу на практику к 10:00."));
+        Assert.assertTrue(RegularExpression.TimeCheck("В понедельник 4 пары с 10:00 до 17:15."));
+        Assert.assertTrue(RegularExpression.TimeCheck("В понедельник 4 пары с 10:00 до 17:15."));
+        Assert.assertTrue(RegularExpression.TimeCheck("В среду одна физра с 11:45."));
+
+        Assert.assertFalse(RegularExpression.TimeCheck(" 37:89 "));
+        Assert.assertFalse(RegularExpression.TimeCheck(" 37:89 "));
+        Assert.assertFalse(RegularExpression.TimeCheck("24:00"));
+        Assert.assertFalse(RegularExpression.TimeCheck("25:00"));
+        Assert.assertFalse(RegularExpression.TimeCheck("33:00"));
+        Assert.assertFalse(RegularExpression.TimeCheck("12:61"));
+        Assert.assertFalse(RegularExpression.TimeCheck("56:12"));
+
     }
 }
